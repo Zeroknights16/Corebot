2a77dda019d02087eb88d8bd9dee45541fe3faaa
Zeroknights <73501749+Zeroknights16@users.noreply.github.com>
2021-11-20 17:35:30 +0100

    Add files via upload

---
+++ modules/variables.js
@@ -0,0 +1,14 @@
+const Utils = require('./utils.js');
+
+module.exports = {
+    set: function (variable, value, expireAfter = 0) {
+        if (variable == 'set') return Utils.error('Cannot set variable \'set\'');
+        this[variable] = value;
+        if (expireAfter > 0)
+            setTimeout(function () {
+                delete this[variable];
+            }, expireAfter);
+        return value;
+    }
+}
+// 203265   8501   2228469    63250   1622514811   5ca616571bef5cf6cc5dc5f13623f65d70d8022f   2228469
